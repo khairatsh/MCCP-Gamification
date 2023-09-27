abcacdb87df3ac3f78d5d656b4e0d674d1716089
khairatsh <85008480+khairatsh@users.noreply.github.com>
2023-09-27 21:44:29 +0300

    Add files via upload

---
+++ js/deltateam.js
@@ -0,0 +1,24 @@
+//Sherif Hani Edit On
+
+//coverage at planned frequenct
+document.getElementById("delta-freq-pie").style.strokeDasharray = "25,100";
+document.getElementById("delta-freq").innerHTML = "30%";
+//attainment
+document.getElementById("delta-att-pie").style.strokeDasharray = "30,100";
+document.getElementById("delta-att").innerHTML = "30%";
+//coverage
+document.getElementById("delta-coverage").style.width = "30%";
+document.getElementById("delta-coverage").style.backgroundColor = "#dc3545"; //#198754 ==>  green  |  #ffc107 ==> yellow  |  #dc3545  ==> red
+document.getElementById("delta-coverage").innerHTML = "30%";
+//consent
+document.getElementById("delta-consent").style.width = "70%";
+document.getElementById("delta-consent").style.backgroundColor = "#198754"; //#198754 ==>  green  |  #ffc107 ==> yellow  |  #dc3545  ==> red
+document.getElementById("delta-consent").innerHTML = "70%";
+//CLM
+document.getElementById("delta-CLM").style.width = "30%";
+document.getElementById("delta-CLM").style.backgroundColor = "#dc3545"; //#198754 ==>  green  |  #ffc107 ==> yellow  |  #dc3545  ==> red
+document.getElementById("delta-CLM").innerHTML = "30%";
+//consent
+document.getElementById("delta-timeelapsed").style.width = "70%";
+document.getElementById("delta-timeelapsed").style.backgroundColor = "#198754"; //#198754 ==>  green  |  #ffc107 ==> yellow  |  #dc3545  ==> red
+document.getElementById("delta-timeelapsed").innerHTML = "70%";
---
+++ js/main.js
@@ -0,0 +1,72 @@
+
+
+//Buttons
+
+document.getElementById("amany_fadl_data").style.display = "none";
+document.getElementById("amira_adel_data").style.display = "none";
+document.getElementById("marina_ibrahim_data").style.display = "none";
+document.getElementById("menna_moustafa_data").style.display = "none";
+document.getElementById("mohamed_shoaib_data").style.display = "none";
+document.getElementById("sherif_hani_data").style.display = "none";
+
+
+function displayAmany(){
+    document.getElementById("amany_fadl_data").style.display = "flex";
+    document.getElementById("amira_adel_data").style.display = "none";
+    document.getElementById("marina_ibrahim_data").style.display = "none";
+    document.getElementById("menna_moustafa_data").style.display = "none";
+    document.getElementById("mohamed_shoaib_data").style.display = "none";
+    document.getElementById("sherif_hani_data").style.display = "none";
+    document.getElementById("delta_team_data").style.display = "none";
+}
+
+function displayAmira(){
+    document.getElementById("amany_fadl_data").style.display = "none";
+    document.getElementById("amira_adel_data").style.display = "flex";
+    document.getElementById("marina_ibrahim_data").style.display = "none";
+    document.getElementById("menna_moustafa_data").style.display = "none";
+    document.getElementById("mohamed_shoaib_data").style.display = "none";
+    document.getElementById("sherif_hani_data").style.display = "none";
+    document.getElementById("delta_team_data").style.display = "none";
+}
+
+function displayMarina(){
+    document.getElementById("amany_fadl_data").style.display = "none";
+    document.getElementById("amira_adel_data").style.display = "none";
+    document.getElementById("marina_ibrahim_data").style.display = "flex";
+    document.getElementById("menna_moustafa_data").style.display = "none";
+    document.getElementById("mohamed_shoaib_data").style.display = "none";
+    document.getElementById("sherif_hani_data").style.display = "none";
+    document.getElementById("delta_team_data").style.display = "none";
+}
+
+function displayMenna(){
+    document.getElementById("amany_fadl_data").style.display = "none";
+    document.getElementById("amira_adel_data").style.display = "none";
+    document.getElementById("marina_ibrahim_data").style.display = "none";
+    document.getElementById("menna_moustafa_data").style.display = "flex";
+    document.getElementById("mohamed_shoaib_data").style.display = "none";
+    document.getElementById("sherif_hani_data").style.display = "none";
+    document.getElementById("delta_team_data").style.display = "none";
+}
+
+function displayShoaib(){
+    document.getElementById("amany_fadl_data").style.display = "none";
+    document.getElementById("amira_adel_data").style.display = "none";
+    document.getElementById("marina_ibrahim_data").style.display = "none";
+    document.getElementById("menna_moustafa_data").style.display = "none";
+    document.getElementById("mohamed_shoaib_data").style.display = "flex";
+    document.getElementById("sherif_hani_data").style.display = "none";
+    document.getElementById("delta_team_data").style.display = "none";
+}
+
+function displaySherif(){
+    document.getElementById("amany_fadl_data").style.display = "none";
+    document.getElementById("amira_adel_data").style.display = "none";
+    document.getElementById("marina_ibrahim_data").style.display = "none";
+    document.getElementById("menna_moustafa_data").style.display = "none";
+    document.getElementById("mohamed_shoaib_data").style.display = "none";
+    document.getElementById("sherif_hani_data").style.display = "flex";
+    document.getElementById("delta_team_data").style.display = "none";
+}
+
