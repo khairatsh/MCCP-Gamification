ce30c83d85abaf7bff56fc05279ea6b4d7ba71a7
khairatsh <85008480+khairatsh@users.noreply.github.com>
2023-09-27 21:57:13 +0300

    Add files via upload

---
+++ js/amiraadel.js
@@ -0,0 +1,24 @@
+//Sherif Hani Edit On
+
+//coverage at planned frequenct
+document.getElementById("amira-freq-pie").style.strokeDasharray = "25,100";
+document.getElementById("amira-freq").innerHTML = "30%";
+//attainment
+document.getElementById("amira-att-pie").style.strokeDasharray = "30,100";
+document.getElementById("amira-att").innerHTML = "30%";
+//coverage
+document.getElementById("amira-coverage").style.width = "30%";
+document.getElementById("amira-coverage").style.backgroundColor = "#dc3545"; //#198754 ==>  green  |  #ffc107 ==> yellow  |  #dc3545  ==> red
+document.getElementById("amira-coverage").innerHTML = "30%";
+//consent
+document.getElementById("amira-consent").style.width = "70%";
+document.getElementById("amira-consent").style.backgroundColor = "#198754"; //#198754 ==>  green  |  #ffc107 ==> yellow  |  #dc3545  ==> red
+document.getElementById("amira-consent").innerHTML = "70%";
+//CLM
+document.getElementById("amira-CLM").style.width = "30%";
+document.getElementById("amira-CLM").style.backgroundColor = "#dc3545"; //#198754 ==>  green  |  #ffc107 ==> yellow  |  #dc3545  ==> red
+document.getElementById("amira-CLM").innerHTML = "30%";
+//consent
+document.getElementById("amira-timeelapsed").style.width = "70%";
+document.getElementById("amira-timeelapsed").style.backgroundColor = "#198754"; //#198754 ==>  green  |  #ffc107 ==> yellow  |  #dc3545  ==> red
+document.getElementById("amira-timeelapsed").innerHTML = "70%";
